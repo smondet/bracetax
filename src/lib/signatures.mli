4ec53171f776b2722d2cdd17d04c9b877a48c278
Sebastien Mondet <sebmdt@users.berlios.de>
2008-08-19 16:34:05 +0800

    Added the future state of the transformer.

--- src/lib/signatures.mli
+++ src/lib/signatures.mli
@@ -1,4 +1,17 @@
 
+type environnement =
+    | Comment
+    | Bold
+    | Italic
+    | Type
+    | Section of (int * string)
+
+type state = {
+    s_line: int;
+    s_char: int;
+    s_stack: environnement list;
+}
+
 module type PRINTER = sig
     type t
     val create: unit -> t
