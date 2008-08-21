1c6af8d5571602820f53cfb863f7e91a23d67339
Sebastien Mondet <sebmdt@users.berlios.de>
2008-08-21 23:53:00 +0800

    Started module Escape while implementing te sanitization of XML comments.

---
+++ src/lib/escape.ml
@@ -0,0 +1,49 @@
+
+let (~%) = Printf.sprintf
+let p = print_string
+let string_of_char = String.make 1
+
+let replace_string ~src ~find ~replace_with = (
+    let l_src = String.length src in
+    let l_find = String.length find in
+    let res_buf = Buffer.create (2 * l_src) in
+    let i = ref  0  in
+    while !i < l_src  do
+        if (!i <= l_src - l_find) && (String.sub src !i l_find = find) then  (
+            Buffer.add_string res_buf replace_with;
+            i := !i + l_find;
+        ) else (
+            Buffer.add_char res_buf src.[!i];
+            incr i;
+        )
+    done;
+    (* Buffer.add_substring res_buf src (l_src - l_find + 1) (l_find - 1); *)
+    Buffer.contents res_buf
+)
+
+let replace_chars ~src ~patterns = (
+    let l_src = String.length src in
+    let res_buf = Buffer.create (2 * l_src) in
+    let rec parse_list cur = function
+        | [] -> string_of_char cur
+        | (c, replace) :: t ->
+            if c = cur then replace else parse_list cur t
+    in
+    for i = 0 to l_src - 1 do
+        Buffer.add_string res_buf (parse_list src.[i] patterns);
+    done;
+    Buffer.contents res_buf
+)
+(* 
+
+# let src = "azerty<uiop>qsdf&ghj<>&;jklm";;
+val src : string = "azerty<uiop>qsdf&ghj<>&;jklm"
+# let patterns = [ ('<', "&lt;"); ('>', "&gt;"); ('&', "&amp;")];;
+val patterns : (char * string) list =
+      [('<', "&lt;"); ('>', "&gt;"); ('&', "&amp;")]
+      # replace_chars ~src ~patterns;;
+- : string = "azerty&lt;uiop&gt;qsdf&amp;ghj&lt;&gt;&amp;;jklm"
+#
+
+ *)
+
--- src/lib/htmlPrinter.ml
+++ src/lib/htmlPrinter.ml
@@ -12,8 +12,10 @@ let create ~write = {stack = GS.empty (); write = write;}
 
 let strstat s = (~% "[%d:%d]" s.Signatures.s_line s.Signatures.s_char)
 
+let sanitize_comments line = Escape.replace_string ~src:line ~find:"-->" ~replace_with:"XXX"
+
 let handle_comment_line t location line = (
-    t.write (~% "<!--%s-->\n" line) (* TODO sanitize *)
+    t.write (~% "<!--%s-->\n" (sanitize_comments line)) (* TODO sanitize *)
 )
 
 let handle_text t location line = ()
