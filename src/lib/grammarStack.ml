12a6739ce52a64af6a7889877c341e16898c3f3d
Sebastien Mondet <sebmdt@users.berlios.de>
2008-08-22 15:03:30 +0800

    Started to define the command 'official' names

--- src/lib/grammarStack.ml
+++ src/lib/grammarStack.ml
@@ -1,12 +1,12 @@
 
-type command = [
+type environment = [
     | `italic
     | `bold
     | `typeWriter
     | `verbatim of string list
 ]
 
-type t = command list ref
+type t = environment list ref
 
 let empty () = ref []
 
@@ -16,6 +16,30 @@ let pop s = match !s with [] -> None | h :: t -> s:= t; Some h
 
 let head s = match !s with [] -> None | h :: t -> Some h
 
+module Commands = struct
 
+    let italic = "i"
+    let bold = "b"
+    let mono_space = "t"
+    let under_line = "u"
+    let superscript = "sup"
+    let subscript = "sub"
+    let paragraph = "p"
+    let new_line = "br"
+
+    let quotation = "q"
+    (* arg1: ' | " | fr | de | sp, default '"' (en) (fr = sp + nbsp's)
+     * http://en.wikipedia.org/wiki/Quotation_mark_glyphs
+     * http://en.wikipedia.org/wiki/Quotation_mark,_non-English_usage
+     * *)
+
+    let non_break_space = "~"
+    let open_brace = "{"
+    let close_brace = "}"
+    let sharp = "#"
+    let utf8_char = "utf" (* arg1:ocaml int *)
+
+
+end
 
 
