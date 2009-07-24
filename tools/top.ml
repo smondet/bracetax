6dcc0f05d459a82199913d24e68f194b5c964f12
Sebastien Mondet <sebmdt@users.berlios.de>
2009-07-24 19:28:59 +0200

    tools.top: Updated the toplevel to newgen.

--- tools/top.ml
+++ tools/top.ml
@@ -7,7 +7,6 @@ let spr = Printf.sprintf ;;
 #directory "_build/src/lib/";;
 #directory "_build/src/app/";;
 #load "_build/src/lib/ocamlbracetax.cma";;
-#load "_build/src/app/postProcessor.cmo";;
 
 pr "\n   Welcome to the Bracetax-loaded toplevel\n";;
 
